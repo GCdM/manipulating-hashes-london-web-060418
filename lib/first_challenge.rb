8e3346218beb72730062256bc8f3df5fb12d4ffe
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-26 18:14:24 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,9 +15,7 @@ def first_challenge
 
   #your code here
   contacts.each do |name, info|
-    if info[:favorite_icecream_flavors].include?("strawberry")
-      
-    end
+    info[:favorite_icecream_flavors].delete_if(this.include?("strawberry"))
   end
 
   #remember to return your newly altered contacts hash!
